9aa391738d00fb2f4295f4eae625a2196467aae2
irakli15 <irakli.mghe@gmail.com>
2022-06-18 16:43:09 -0500

    refactoring for testing - separated specific config and startup code

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { NotFoundError } from './error/not-found-error';
-import { errorHandler } from './middlewares/error-handler';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { validateRequest } from './middlewares/validate-request';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
-app.use(validateRequest);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
