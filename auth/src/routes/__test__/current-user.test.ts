159c8441a92c8746eb71fc0e20a1f0e9eea88251
irakli15 <irakli.mghe@gmail.com>
2022-06-19 21:52:08 -0500

    add current-user route tests

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('responds with details of current user', async () => {
+  const cookie = await global.signin();
+
+  const currentUserResponse = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+  expect(currentUserResponse.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+  expect(response.body.currentUser).toEqual(null);
+});
